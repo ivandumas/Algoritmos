16cbb98de7ace2aded38fbda9d134b6ad9d6c706
Iván Alejandro Dumas Martínez <ivan.dumas@hotmail.com>
2021-08-10 09:46:47 -0500

    Create brackets.py

---
+++ brackets.py
@@ -0,0 +1,12 @@
+# Recibes una string conteniendo solo () y []
+# Crea un algoritmos para validar que la sintaxis de los corchetes es correcta:
+# Ejemplos validos:
+# '()'
+# '()()()()()()()()()'
+# '([])[]()'
+# Ejemplos no validos:
+# ')'
+# '()['
+# '(([))]'
+# '(((((((((((((((((((((((((((((((((('
+Sube la url de un archivo de github
